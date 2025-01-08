57a60ae8579a4a214280345957299e625b9be74d
Bilal <bilal.gurani@utopusinsights.com>
2025-01-08 19:09:18 +0530

    08-01-25 Input/Output decorators

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'input-output-decorators' title`, () => {
+  it(`should have the 'angular-practice-01-25' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('input-output-decorators');
+    expect(app.title).toEqual('angular-practice-01-25');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, input-output-decorators');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-practice-01-25');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ParentComponent} from './Input-Output decorators/parent/parent.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
