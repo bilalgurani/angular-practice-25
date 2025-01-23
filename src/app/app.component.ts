26101ec107b168542763b9f6528f29fb0d6b8cf8
Bilal <bilal.gurani@utopusinsights.com>
2025-01-23 14:08:00 +0530

    23-01-25 HTTP request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {HttpRequestResponseComponent} from './http-request-response/http-request-response.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    HttpRequestResponseComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -10,13 +13,11 @@ export class AppComponent {
   title = 'input-output-decorators';
   openWindow: boolean = false;
 
-
   openForm() {
     this.openWindow = true;
   }
 
   closeWindow($event: boolean) {
-    console.log($event);
     this.openWindow = $event
   }
 
--- src/app/http-request-response/http-request-response.component.ts
+++ src/app/http-request-response/http-request-response.component.ts
@@ -1,8 +1,14 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {User} from '../models/user';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-http-request-response',
   imports: [
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './http-request-response.component.html',
   styleUrl: './http-request-response.component.scss'
@@ -10,9 +16,30 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 export class HttpRequestResponseComponent {
   @Input() isWindowOpen: boolean | undefined;
   @Output() isWindowClosed: EventEmitter<boolean> = new EventEmitter<boolean>();
+  http: HttpClient = inject(HttpClient);
+
+  registrationForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    lName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    terms: new FormControl(false, [Validators.required]),
+  })
 
   closeForm() {
     console.log("close form");
     this.isWindowClosed.emit(false);
+    this.registrationForm.reset();
+  }
+
+  saveUser() {
+    if (this.registrationForm.valid) {
+      const user: User = this.registrationForm.value as User;
+      this.http.post("https://angular-project-25-default-rtdb.firebaseio.com/users.json", user)
+      .subscribe(() => {
+        this.closeForm();
+      })
+    }
+
   }
 }
